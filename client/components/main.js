eb4131af4862ac6a7b262c19bafd2bb076b3e427
northPostal <north.postal@gmail.com>
2020-06-10 07:38:20 +0300

    maim.js edited

--- client/components/main.js
+++ client/components/main.js
@@ -15,8 +15,9 @@ const Main = () => {
       {/* <div>{value}</div> */}
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-black font-bold rounded-lg border shadow-lg p-10">
-          <input value={value} onChange={onChange} className="mr-3" type="text" />
+          <input id="input-field" value={value} onChange={onChange} className="mr-3" type="text" />
           <button
+            id="search-button"
             className="bg-indigo-500 text-white font-bold rounded-lg shadow-lg p-2"
             type="button"
             onClick={onClick}
